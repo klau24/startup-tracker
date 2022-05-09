b0d43db1ca9f1a405652a529dafa097ab1adcec7
Kenny Lau <klau24@calpoly.edu>
2022-05-08 23:42:10 -0700

    navbar styling and responsive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar/Navbar';
+import NavDropdown from './components//navbar/NavDropdown';
+import Home from './components/pages/Home';
+import Dashboard from './components/pages/Dashboard';
+import Screening from './components/pages/Screening';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener('resize', hideMenu);
+
+    return () => {
+      window.removeEventListener('resize', hideMenu);
+    };
+  });
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar toggle={toggle} />
+      <NavDropdown isOpen={isOpen} toggle={toggle} />
+      <Routes>
+        <Route path='/' exact element={<Home/>} />
+        <Route path='/dashboard' exact element={<Dashboard/>} />
+        <Route path='/screening' exact element={<Screening/>} />
+        <Route path='/about' exact element={<About/>} />
+        <Route path='/contact' exact element={<Contact/>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/navbar/NavSearchbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { SearchIcon } from '@heroicons/react/outline'
+
+function NavSearchbar() {
+  return (
+    <div>
+        <form action=''>
+            <div className='relative flex items-center text-gray-400 focus-within:text-gray-600'>
+                <SearchIcon className='h-5 w-5 absolute ml-3 pointer-events-none'/>
+                <input type='text' name='search' placeholder='Search startup' autocomplete='off' aria-label='Search startup' 
+                className='pr-3 py-2 pl-10 font-semibold placeholder-gray-500 text-black rounded-2xl ring-2 ring-gray-300 '>
+                </input>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default NavSearchbar
