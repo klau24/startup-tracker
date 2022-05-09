04d31e6760becf623be7f877003a12f0504eab86
Kenny Lau <klau24@calpoly.edu>
2022-05-08 21:58:39 -0700

    navbar created

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav className='flex justify-between items-center h-16 bg-white text-black relative shadow-sm font-mono' role='navigation'>
+        <Link to='/' className='pl-8'>startup-tracker</Link>
+        <div className='px-4 cursor-pointer md:hidden'>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+        </div> 
+        <div className='pr-8 md:block hidden'>
+            <Link className='p-4' to='/'>Home</Link>
+            <Link className='p-4' to='/dashboard'>Dashboard</Link>
+            <Link className='p-4' to='/screening'>Screening</Link>
+            <Link className='p-4' to='/about'>About</Link>
+            <Link className='p-4' to='/contact'>Contact</Link>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
