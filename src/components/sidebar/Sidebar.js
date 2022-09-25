d6f1bd28f11fb969251d1e7fd7337a35cf32414b
Kenny Lau <klau24@calpoly.edu>
2022-09-25 13:08:59 -0700

    refactored sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,23 +1,88 @@
+import * as React from 'react'
 import { Link } from 'react-router-dom'
 import { SidebarData } from './SidebarData'
+import Box from '@mui/material/Box'
+import Drawer from '@mui/material/Drawer'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import Toolbar from '@mui/material/Toolbar'
 
-function Sidebar() {
+const drawerWidth = 240
+
+function Sidebar(props) {
+   const { window } = props
+   const [mobileOpen, setMobileOpen] = React.useState(false)
+
+   const handleDrawerToggle = () => {
+      setMobileOpen(!mobileOpen)
+   }
+   const container =
+      window !== undefined ? () => window().document.body : undefined
+
+   const drawer = (
+      <div>
+         <Toolbar />
+         <List>
+            {SidebarData.map((item, index) => (
+               <ListItem key={item} sx={{ padding: 2 }}>
+                  <ListItemButton>
+                     <ListItemIcon>{<item.icon />}</ListItemIcon>
+                     <Link to={item.link} key={index}>
+                        {item.title}
+                     </Link>
+                  </ListItemButton>
+               </ListItem>
+            ))}
+         </List>
+      </div>
+   )
+
    return (
-      <nav className="h-screen w-1/6 font-mono bg-stSidebar">
-         <ul className="h-auto w-full p-0 text-center">
-            {SidebarData.map((item, key) => {
-               return (
-                  <Link
-                     className="w-full h-14 m-0 flex flex-row list-none justify-center text-left items-center hover:bg-white hover:cursor-pointer"
-                     to={item.link}
-                     key={key}
-                  >
-                     {item.title}
-                  </Link>
-               )
-            })}
-         </ul>
-      </nav>
+      <Box
+         component="nav"
+         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+      >
+         {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+         <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+               keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+               display: { xs: 'block', sm: 'none' },
+               '& .MuiDrawer-paper': {
+                  boxSizing: 'border-box',
+                  width: drawerWidth,
+               },
+            }}
+         >
+            {drawer}
+         </Drawer>
+         <Drawer
+            variant="permanent"
+            sx={{
+               display: { xs: 'none', sm: 'block' },
+               '& .MuiDrawer-paper': {
+                  boxSizing: 'border-box',
+                  width: drawerWidth,
+               },
+            }}
+            anchor="bottom"
+            open
+            PaperProps={{
+               sx: {
+                  height: '94.13%',
+               },
+            }}
+         >
+            {drawer}
+         </Drawer>
+      </Box>
    )
 }
 
--- src/components/sidebar/SidebarData.js
+++ src/components/sidebar/SidebarData.js
@@ -1,22 +1,33 @@
+import TwitterIcon from '@mui/icons-material/Twitter'
+import MessageIcon from '@mui/icons-material/Message'
+import NewspaperIcon from '@mui/icons-material/Newspaper'
+import LocationOnIcon from '@mui/icons-material/LocationOn'
+import LanguageIcon from '@mui/icons-material/Language'
+
 export const SidebarData = [
    {
       title: "Company's Twitter",
+      icon: TwitterIcon,
       link: '/company-twitter',
    },
    {
       title: 'Tweet Contents',
+      icon: MessageIcon,
       link: '/tweet-content',
    },
    {
       title: 'Media Mentions',
+      icon: NewspaperIcon,
       link: '/media-mentions',
    },
    {
       title: 'Geolocation',
+      icon: LocationOnIcon,
       link: '/geolocation',
    },
    {
       title: 'Website Traffic',
+      icon: LanguageIcon,
       link: '/website-traffic',
    },
 ]
