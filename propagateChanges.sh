24a35c2725c4a0395e7e49be7ba062b377efd91c
Kenny Lau <klau24@calpoly.edu>
2023-01-10 15:41:16 -0800

    new build

--- propagateChanges.sh
+++ propagateChanges.sh
@@ -4,4 +4,4 @@ npm run build && rm -rf backend/client/* && mv build/* backend/client/
 git add --all
 git commit -m "new build"
 git push
-ssh -i /Users/Kenny/.ssh/id_rsa kenny@104.196.224.131 'cd /var/www/html/startup-tracker/ && sudo git pull | pm2 stop 1'
+ssh -i /Users/Kenny/.ssh/id_rsa kenny@104.196.224.131 'cd /var/www/html/startup-tracker/ && sudo git pull | pm2 restart 1'
