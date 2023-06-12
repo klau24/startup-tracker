a07a218833dba11ab2e42e9a34f77f0f07941291
nhausmna <nhausman325@gmail.com>
2023-06-11 22:18:08 -0700

    new build

--- propagateChanges.sh
+++ propagateChanges.sh
@@ -4,4 +4,4 @@ npm run build && rm -rf backend/client/* && mv build/* backend/client/
 git add --all
 git commit -m "new build"
 git push
-ssh -i /Users/nickhausman/.ssh/id_rsa nhausman325@104.196.224.131 'cd /var/www/html/startup-tracker/ && sudo git pull && pm2 restart 1'
+ssh -i /Users/nickhausman/.ssh/id_rsa nhausman325@104.196.224.131 'cd /var/www/html/startup-tracker/ && sudo git pull && pm2 restart 0'
